cc1ab8da1b6918ab23bb5c95d1837ba7adbf87a9
lauradobson <ldobson@uwaterloo.ca>
2016-09-19 16:35:01 -0400

    Adding Personal Website
    Using modified bootstrap templates to create personal website

---
+++ js/easypiechart-data.js
@@ -0,0 +1,100 @@
+$(function() {
+    $('#easypiechart-teal').easyPieChart({
+        scaleColor: false,
+        barColor: '#1ebfae'
+    });
+});
+
+$(function() {
+    $('#easypiechart-teal2').easyPieChart({
+        scaleColor: false,
+        barColor: '#1ebfae'
+    });
+});
+
+$(function() {
+    $('#easypiechart-teal3').easyPieChart({
+        scaleColor: false,
+        barColor: '#1ebfae'
+    });
+});
+
+$(function() {
+    $('#easypiechart-teal4').easyPieChart({
+        scaleColor: false,
+        barColor: '#1ebfae'
+    });
+});
+
+$(function() {
+    $('#easypiechart-orange').easyPieChart({
+        scaleColor: false,
+        barColor: '#ffb53e'
+    });
+});
+
+$(function() {
+    $('#easypiechart-red').easyPieChart({
+        scaleColor: false,
+        barColor: '#f9243f'
+    });
+});
+
+$(function() {
+   $('#easypiechart-blue').easyPieChart({
+       scaleColor: false,
+       barColor: '#30a5ff'
+   });
+});
+
+$(function() {
+   $('#easypiechart-blue2').easyPieChart({
+       scaleColor: false,
+       barColor: '#30a5ff'
+   });
+});
+
+$(function() {
+   $('#easypiechart-blue3').easyPieChart({
+       scaleColor: false,
+       barColor: '#30a5ff'
+   });
+});
+
+$(function() {
+   $('#easypiechart-blue4').easyPieChart({
+       scaleColor: false,
+       barColor: '#30a5ff'
+   });
+});
+
+$(function() {
+    $('#easypiechart-purple').easyPieChart({
+        scaleColor: false,
+        barColor: '#b27fb2'
+    });
+});
+
+$(function() {
+    $('#easypiechart-purple2').easyPieChart({
+        scaleColor: false,
+        barColor: '#b27fb2'
+    });
+});
+
+$(function() {
+    $('#easypiechart-purple3').easyPieChart({
+        scaleColor: false,
+        barColor: '#b27fb2'
+    });
+});
+
+$(function() {
+    $('#easypiechart-purple4').easyPieChart({
+        scaleColor: false,
+        barColor: '#b27fb2'
+    });
+});
+
+$('#calendar').datepicker({
+	});
